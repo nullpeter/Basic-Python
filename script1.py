a24ed6173b7436b129cabbd33e433652f8b9f058
Peter Pyne <pyne.peter@gmail.com>
2017-09-04 16:22:28 -0400

    Initial commit.

---
+++ script1.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import sys                  # Load a library module
+print(sys.platform)
+print(2 ** 100)             # Raise 2 to a power
+x = 'Spam!'
+print(x * 8)                # String repetition
